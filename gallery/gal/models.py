2286d28e8e8b92a7e85ad4446f9575439f4fb2c4
Joyce <wamungai29@gmail.com>
2020-01-31 14:13:00 +0300

    Create category and location models.

--- gallery/gal/models.py
+++ gallery/gal/models.py
@@ -2,10 +2,17 @@ from django.db import models
 import datetime as dt
 
 # Create your models here.
-class Image(models.model):
+class Image(models.Model):
     photo_image=models.ImageField(upload_to='images/')
     image_name=models.CharField(max_length=30)
     description=models.TextField(max_length=60)
     category=models.ForeignKey(Category)
     location=models.ForeignKey(Location)
     pub_date=models.DateTimeField(auto_now_add=True)
+    
+    
+class Category(models.Model):
+    category_name=models.CharField(max_length=30)
+    
+class Location(models.Model):
+    location_name=models.CharField(max_length=30)
