c443bd59f51223319e9a0e47eff2b7006172e001
Joyce <wamungai29@gmail.com>
2020-01-31 14:17:47 +0300

    Install pillow.

--- gallery/gal/models.py
+++ gallery/gal/models.py
@@ -2,6 +2,13 @@ from django.db import models
 import datetime as dt
 
 # Create your models here.
+    
+class Category(models.Model):
+    category_name=models.CharField(max_length=30)
+    
+class Location(models.Model):
+    location_name=models.CharField(max_length=30)
+
 class Image(models.Model):
     photo_image=models.ImageField(upload_to='images/')
     image_name=models.CharField(max_length=30)
@@ -10,9 +17,3 @@ class Image(models.Model):
     location=models.ForeignKey(Location)
     pub_date=models.DateTimeField(auto_now_add=True)
     
-    
-class Category(models.Model):
-    category_name=models.CharField(max_length=30)
-    
-class Location(models.Model):
-    location_name=models.CharField(max_length=30)
