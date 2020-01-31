79c0d222eb4276817fd0bd1b1fd7ff1787aac2bb
Joyce <wamungai29@gmail.com>
2020-01-31 15:57:48 +0300

    Create and run test files.

--- gallery/gal/models.py
+++ gallery/gal/models.py
@@ -6,8 +6,27 @@ import datetime as dt
 class Category(models.Model):
     category_name=models.CharField(max_length=30)
     
+    def __str__(self):
+        return self.category_name
+    
+    def save_category(self):
+        self.save()
+        
+    def get_category(self):
+        return Category.select().where(Category.category_name==self)
+     
+    
 class Location(models.Model):
     location_name=models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.location_name
+    
+    def save_location(self):
+        self.save()
+        
+    def get_location(self):
+        return Location.select().where(Location.location_name==self)
 
 class Image(models.Model):
     photo_image=models.ImageField(upload_to='images/')
@@ -19,4 +38,8 @@ class Image(models.Model):
     
     def __str__(self):
         return self.image_name
+    
+    def save_image(self):
+        self.save()
+    
     
--- gallery/gal/tests.py
+++ gallery/gal/tests.py
@@ -1,3 +1,55 @@
 from django.test import TestCase
+import datetime as dt 
+from . models import Category,Location,Image
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.travel=Category(category_name='plane')
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.travel,Category))
+        
+    def test_save_method(self):
+        self.travel.save_category()
+        categories=Category.objects.all()
+        self.assertTrue(len(categories)>0) 
+        
+       
+    def test_get_category(self):
+        self.travel.save_category()
+        category=Category.get_category
+        self.assertTrue(category is not None)
+        
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.nairobi=Location(location_name='kilimani')
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nairobi,Location)) 
+        
+    def test_save_location(self):
+        self.nairobi.save_location()
+        locations=Location.objects.all()
+        self.assertTrue(len(locations)>0)
+        
+    def test_get_location(self):
+        self.nairobi.save_location()
+        location=Location.get_location
+        self.assertTrue(location is not None)
+        
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.nairobi=Location(location_name='kilimani')
+        self.travel=Category(category_name='plane')
+        self.nairobi.save_location()
+        self.travel.save_category()
+        self.img=Image(photo_image='food.jpg',image_name='food', description='food porn',category=self.travel,location=self.nairobi)
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.img,Image))
+        
+    def test_save_photo(self):
+        self.img.save_image()
+        images=Image.objects.all()
+        self.assertTrue(len(images)>0)
