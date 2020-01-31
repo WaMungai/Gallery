e5f64cac3fa31e3656e4d2d5b03ccf8db51b4d0f
Joyce <wamungai29@gmail.com>
2020-01-31 05:35:04 +0300

    Create base file that will be used as a blueprint for other templates.

--- gallery/gal/views.py
+++ gallery/gal/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def welcome(request):
     photos = Image.display_photos()
-    return render(request,'welcpme.html',{"photos":photos})
+    return render(request,'welcome.html'})
     
