b6ea448df4a5d65081349da60dfb4ec306a12870
Joyce <wamungai29@gmail.com>
2020-01-31 14:08:01 +0300

    Create image model properties.

--- gallery/gal/models.py
+++ gallery/gal/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Image(models.model):
+    photo_image=models.ImageField(upload_to='images/')
+    image_name=models.CharField(max_length=30)
+    description=models.TextField(max_length=60)
+    category=models.ForeignKey(Category)
+    location=models.ForeignKey(Location)
+    pub_date=models.DateTimeField(auto_now_add=True)
