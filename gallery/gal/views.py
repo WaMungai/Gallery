ca190917e277a6bc9b7d5a0f177ccb45143de274
Joyce <wamungai29@gmail.com>
2020-01-31 05:12:09 +0300

    Connect template with view function.

--- gallery/gal/views.py
+++ gallery/gal/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def welcome(request):
+    photos = Image.display_photos()
+    return render(request,'welcpme.html',{"photos":photos})
+    
