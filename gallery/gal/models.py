f842c91c74d0a62b5b41d270293d487adc610ad4
Joyce <wamungai29@gmail.com>
2020-02-03 15:03:36 +0300

    Define method that will query db to fetch results

--- gallery/gal/models.py
+++ gallery/gal/models.py
@@ -54,6 +54,11 @@ class Image(models.Model):
     @classmethod
     def update_photo(cls,id):
         
+    @classmethod
+    def search_by_category(cls,search_term):
+        photos = cls.objects.filter(category__category_name__icontains=search_term)
+        return photos
+        
         
     
     
