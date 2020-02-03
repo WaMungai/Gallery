394e92402d6d5e8ee20549b7efa4695abeb82e05
Joyce <wamungai29@gmail.com>
2020-02-03 15:09:23 +0300

    Create view function that handles logic for displaying the search by location results

--- gallery/gal/views.py
+++ gallery/gal/views.py
@@ -15,7 +15,7 @@ def singlephoto(request,photoid):
     return render(request,'photodetail.html',{"photo":singlephoto})
     
     
-def _category(request):
+def search_category(request):
     if 'photocategory' in request.GET and request.GET["photocategory"]:
         search_term = request.GET.get("photocategory")
         searched_categories = Image.search_by-category(search_term)
@@ -26,3 +26,15 @@ def _category(request):
     else:
         message="You haven't searched for any category"
         return render(request,'searchcategory.html',{"message":message})
+    
+def search_loaction(request):
+    if 'photolocation' in request.GET and request.GET["photolocation"]:
+        search_term = request.GET.get("photolocation")
+        searched_categories = Image.search_by_category(search_term)
+        message = f"{search_term}"
+        
+        return render(request,'searchlocation.html',{"message":message,"locationphotos":searched_locations})
+    
+    else:
+        message="You haven't searched for any location"
+        return render(request,'searchlocation.html',{"message":message})
