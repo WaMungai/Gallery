a8b9793f37099ba83bee1312ce6d63c5618f61b7
Joyce <wamungai29@gmail.com>
2020-02-03 13:42:23 +0300

    Creates a view function and pass in the integer from the URL.

--- gallery/gal/views.py
+++ gallery/gal/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def welcome(request):
     photos = Image.display_photos()
-    return render(request,'welcome.html'})
+    return render(request,'welcome.html',{"photos":photos}})
+
+def singlephoto(request,photoid):
+    try:
+        singlephoto=Image.objects.get(id=photoid)
+        
+    except DoesNotExist:
+        raiseHttp404()
+    return render(request,'photodetail.html',{"photo":singlephoto})
     
